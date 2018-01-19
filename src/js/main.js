4586cefa1cb422f4441db84053e100c27df359f1
dpmango <xs290@me.com>
2018-01-19 19:52:20 +0200

    mobile video bg fix

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,7 @@ $(document).ready(function(){
   function isMobile(){
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       return true
+      $('.body').addClass('is-mobile')
     } else {
       return false
     }
